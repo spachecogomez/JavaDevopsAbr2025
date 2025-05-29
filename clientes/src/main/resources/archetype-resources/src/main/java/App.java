36cdb52efc45c8b03e0c1aa4b8de823be25dcd11
sebastian.pacheco <spacheco@clippers.com>
2025-05-28 19:41:59 -0500

    pushing new version of the module

---
+++ clientes/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $com.mx.banamex;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
